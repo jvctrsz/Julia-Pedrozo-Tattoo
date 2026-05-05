ff806cf129bd0dfaf32122212c6000548ecd4ce0
jmatiasz <joaosteam26@gmail.com>
2026-05-05 19:51:16 -0400

    melhora texto

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 export const WHATSAPP_PHONE = "5566999300161";
-export const WHATSAPP_MESSAGE =
-  "Olá,%20gostaria%20de%20realizar%20um%20agendamento.";
-export const WHATSAPP_URL = `https://wa.me//${WHATSAPP_PHONE}?text=${WHATSAPP_MESSAGE}`;
+export const WHATSAPP_MESSAGE = encodeURIComponent(
+  "Olá, Julia! Vi seu portfólio e gostaria de conversar sobre uma ideia para minha próxima tatuagem.",
+);
+export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_PHONE}?text=${WHATSAPP_MESSAGE}`;
 
 export const EMAIL = "jpyoarttattoo@gmail.com";
 
