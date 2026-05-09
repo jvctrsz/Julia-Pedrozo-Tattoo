27969328b9851560872e6938e7abd64537579b07
jmatiasz <joaosteam26@gmail.com>
2026-05-08 21:37:20 -0400

    atualiza prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const connectionString = `${process.env.DATABASE_URL}`;
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient;
+  pool: Pool;
+};
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const pool = globalForPrisma.pool ?? new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+  globalForPrisma.pool = pool;
+}
